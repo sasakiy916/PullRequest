a1500f9cc052d6924854d1b864359f80804fc4fb
arukreid0609 <y.sasaki.roppongi@gmail.com>
2023-11-06 19:40:56 +0900

    佐々木のコミット

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,12 +7,12 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("佐々木の変更");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
