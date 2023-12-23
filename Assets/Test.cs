1ce16ae966089f14e58068e7fa5bb3b4d7f9f410
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-12-23 10:02:07 +0900

    pull request テスト

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public string name;
+    public float weight;
     public int age;
     // Start is called before the first frame update
     void Start()
